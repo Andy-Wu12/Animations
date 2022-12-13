947c72d729d709b4fa1861e4f5835b52a3657dba
Andy Wu <awpats12@gmail.com>
2022-12-13 15:49:03 -0500

    Initial commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Andy Wu on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Animations - 100 Days of Swift
+
+## Sixth project in iOS / Mac OS X development journey
+
+## See the [blog](https://www.hackingwithswift.com/books/ios-swiftui/animation-introduction) that I am following
