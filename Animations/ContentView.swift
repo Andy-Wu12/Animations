d46a0da5e51cfdfd0a703acf0bd35faae21b9e68
Andy Wu <awpats12@gmail.com>
2022-12-13 23:58:46 -0500

    Completed explicit animation lesson

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,26 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    var body: some View {
+//        ImplicitAnimation()
+        ExplicitAnimation()
+    }
+}
+
+struct ImplicitAnimation: View {
     @State private var animationAmount = 1.0
+//     Implicit animation
+    var body: some View {
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            
+            Spacer()
+        }
+    }
+}
+
+struct ExplicitAnimation: View {
+    @State private var animationAmount = 0.0
+    
     var body: some View {
         Button("Tap Me") {
-//            animationAmount += 1
+            // withAnimation closure causes changes from new state to automatically be animated
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
+            }
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount)
-        )
-        .onAppear(perform: {animationAmount = 2})
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
